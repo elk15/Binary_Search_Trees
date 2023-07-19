782a3ffdac34b9dfbf5f302a5926406a21b4a667
elina <papadelina145@gmail.com>
2023-07-19 10:33:01 +0300

    Implement insert, delete and find node methods

--- script.js
+++ script.js
@@ -45,7 +45,76 @@ class Tree {
             this.prettyPrint(node.leftChild, `${prefix}${isLeft ? "    " : "│   "}`, true);
         }
     }
+
+    insert(value, node = this.root) {
+        if (value < node.data) {
+            if (node.leftChild === null) {
+                node.leftChild = new Node(value);
+                return;
+            } else {
+                this.insert(value, node.leftChild);
+            }
+        } else {
+            if (node.rightChild === null) {
+                node.rightChild = new Node(value);
+                return;
+            } else {
+                this.insert(value, node.rightChild);
+            }
+        }
+    }
+
+    delete(value, node = this.root) {
+        if (node === null) return node;
+
+        if (value < node.data) {
+            node.leftChild = this.delete(value, node.leftChild);
+            return node;
+        } else if (value > node.data) {
+            node.rightChild = this.delete(value, node.rightChild);
+            return node;
+        }
+
+        // // if it's leaf node with no children
+        if (node.leftChild === null && node.rightChild === null) {
+            return null;
+        }
+
+        // if it only has one child
+        if (node.leftChild === null) {
+            return node.rightChild;
+        } else if (node.rightChild === null) {
+            return node.leftChild;
+        }
+        // if it has two children
+        else {
+            let parent = node;
+            let successor = node.rightChild;
+            while(successor.leftChild !== null) {
+                parent = successor;
+                successor = successor.leftChild;
+            }
+            if(parent === node) {
+                parent.rightChild = successor.rightChild;
+            } else {
+                parent.leftChild = successor.rightChild;
+            }
+            node.data = successor.data;
+
+            return node;
+        }
+    }
+
+    find(value, node = this.root) {
+        if(value < node.data) {
+            return this.find(value, node.leftChild); 
+        } else if (value > node.data) {
+            return this.find(value, node.rightChild); 
+        }
+        return node;
+    }
 }
 
 const tree = new Tree([1, 7, 4, 23, 8, 9, 4, 3, 5, 7, 9, 67, 6345, 324]);
 tree.prettyPrint(tree.getRoot());
+console.log(tree.find(3));

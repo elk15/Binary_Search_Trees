d4a4e1c53fdf97ee9e40f2be2d1134cc89496843
elina <papadelina145@gmail.com>
2023-07-20 11:30:01 +0300

    Implement driver script

--- driver.js
+++ driver.js
@@ -7,9 +7,40 @@ class Driver {
         return this.getRandomArray(length - 1, arr);
     }
 
-    main() {
+    static main() {
+        const randomArr = Driver.getRandomArray(15);
+        const tree = new Tree(randomArr);
+
+        console.log('Original Tree:');
+        tree.prettyPrint(tree.root);
+        console.log(tree.isBalanced());
+
+        console.log(tree.levelOrderRec());
+        console.log(tree.preorder());
+        console.log(tree.postorder());
+        console.log(tree.inorder());
+        console.log('---------------------');
+
+        console.log('Unbalanced Tree:');
+        tree.insert(200);
+        tree.insert(140);
+        tree.insert(1000);
+        tree.insert(3404);
+        tree.prettyPrint(tree.root);
+        console.log(tree.isBalanced());
+        console.log('---------------------');
+
+        console.log('Rebalanced Tree:');
+        tree.rebalance();
+        tree.prettyPrint(tree.root);
+        console.log(tree.isBalanced());
+        console.log(tree.levelOrderRec());
+        console.log(tree.preorder());
+        console.log(tree.postorder());
+        console.log(tree.inorder());
 
     }
 }
 
-console.log(Driver.getRandomArray());
+Driver.main();
+
